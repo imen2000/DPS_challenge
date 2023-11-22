8a487c0bb659884bd8995a7015aa87244319a1ac
Imen Haddar <79281019+imen2000@users.noreply.github.com>
2023-11-22 23:48:47 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 set -o errexit
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
-pip install --upgrade pip
 pip install -r requirements.txt
 pip install gunicorn
 pip install flask
